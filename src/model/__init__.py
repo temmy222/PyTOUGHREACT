b549d27ad89e60d53d8b8779c2eb8ce46f8445a8
temmy222 <ajayi_temmy@yahoo.com>
2022-12-27 02:24:00 -0700

    add license file to every file

---
+++ src/model/__init__.py
@@ -0,0 +1,56 @@
+'''
+ *  This is the default license template.
+ *  
+ *  File: __init__.py
+ *  Author: tajayi3
+ *  Copyright (c) 2022 tajayi3
+ *  
+ *  To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
+'''
+
+"""
+ This is the default license template.
+ 
+ File: __init__.py
+ Author: tajayi3
+ Copyright (c) 2022 tajayi3
+ 
+ To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
+"""
+
+This is the default license template.
+ 
+ File: __init__.py
+ Author: tajayi3
+ Copyright (c) 2022 tajayi3
+ 
+ To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
+"""
+
+This is the default license template.
+ 
+ File: __init__.py
+ Author: tajayi3
+ Copyright (c) 2022 tajayi3
+ 
+ To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
+"""
+
+This is the default license template.
+ 
+ File: __init__.py
+ Author: tajayi3
+ Copyright (c) 2022 tajayi3
+ 
+ To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
+"""
+
+This is the default license template.
+ 
+ File: __init__.py
+ Author: tajayi3
+ Copyright (c) 2022 tajayi3
+ 
+ To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
+"""
+
